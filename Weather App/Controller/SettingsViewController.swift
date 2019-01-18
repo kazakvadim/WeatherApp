54683959c8c6f72a5676eb066bfb5fef97df62a1
kazak_vadim <k.a.z.a.k.2013@mail.ru>
2019-01-18 13:04:41 +0300

    Installed SwiftyJSON, Alamofire using cocoapods

---
+++ Weather App/Controller/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Weather App
+//
+//  Created by Mac OS on 18.01.19.
+//  Copyright © 2019 Vadim Kozachenko. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
