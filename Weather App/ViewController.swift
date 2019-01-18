bb5deb649e359799e7e7ac730159b0bee75ff6f9
kazak_vadim <k.a.z.a.k.2013@mail.ru>
2019-01-18 12:44:12 +0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Mac OS on 18.01.19.
+//  Copyright © 2019 Vadim Kozachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
