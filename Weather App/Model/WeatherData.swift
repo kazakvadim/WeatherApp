1853a1f0f2c0cebdf32b724c9734cddf05ecebe3
kazak_vadim <k.a.z.a.k.2013@mail.ru>
2019-01-18 13:06:49 +0300

    Added model WeatherData

---
+++ Weather App/Model/WeatherData.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherData.swift
+//  Weather App
+//
+//  Created by Mac OS on 18.01.19.
+//  Copyright © 2019 Vadim Kozachenko. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDataModel {
+    var temperature : Int = 0
+    var condition : Int = 0
+    var city : String = ""
+    var weatherIconName : String = ""
+}
+
